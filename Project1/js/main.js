8952188bc58b1ed8586efb8bc3792a86ced5a9cf
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2017-10-23 15:11:35 +0500

    commit

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -1,17 +1,13 @@
 // header-nav
 $(document).ready(function () {
   var sideMenu = $("#menuBar")
-  // var sideMenu = false;
   $("#menuBar").click(function () {
-    // sideMenu = true;
-    console.log($("#menuBar").attr("aria-expanded", "true"))
-    console.log(sideMenu[0])
-    // if ($("#menuBar").attr("aria-expanded", "true")) {
-    //   $(".headerNavForm").hide();
-    // }
-    // if ($("#menuBar").attr("aria-expanded", "false")) {
-    //   $(".headerNavForm").show();
-    // }
-
+    var arr = sideMenu[0].attributes[6]
+    if (arr.value === 'false') {
+      $(".headerNavForm").hide();
+    }
+    else {
+      $(".headerNavForm").show();
+    }
   })
 })
